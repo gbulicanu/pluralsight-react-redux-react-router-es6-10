a2b4da2e51cae2b380910b57a655021db0e13294
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-05-15 17:32:18 +0300

    Rename const to make spell checker happy

--- tools/createMockDb.js
+++ tools/createMockDb.js
@@ -5,8 +5,8 @@ const mockData = require("./mockData");
 
 const { courses, authors } = mockData;
 const data = JSON.stringify({ courses, authors });
-const filepath = path.join(__dirname, "db.json");
+const filePath = path.join(__dirname, "db.json");
 
-fs.writeFile(filepath, data, function(err) {
+fs.writeFile(filePath, data, function (err) {
   err ? console.log(err) : console.log("Mock DB created.");
 });
